fd65ce0d37637100dd5224940768c0a10b5de04f
Aditya Vamsikrishna <adityavk711@gmail.com>
2018-05-17 14:57:53 -0700

    add torque publisher to test responses to torque input functions

---
+++ vesc_driver/src/torque_publisher_node.cpp
@@ -0,0 +1,32 @@
+#include <chrono>
+#include "ros/ros.h"
+#include "std_msgs/Float64.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "torque_publisher");
+  ros::NodeHandle n;
+  ros::Publisher torque_pub = n.advertise<std_msgs::Float64>("torqu_publisher", 1000);
+  ros::Rate loop_rate(10);
+
+  std::chrono::seconds timeout(3);
+
+  std_msgs::Float64 torque_input;
+  std::chrono::time_point<std::chrono::system_clock> start{std::chrono::system_clock::now()};
+  while (ros::ok())
+  {
+    torque_input.data = 0.01;
+    torque_pub.publish(torque_input);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    std::chrono::time_point<std::chrono::system_clock> current{std::chrono::system_clock::now()};
+    std::chrono::duration<double> elapsedSeconds{current-start};
+    if (elapsedSeconds > timeout)
+      break;
+  }
+  torque_input.data = 0.0;
+  torque_pub.publish(torque_input);
+  return 0;
+}
